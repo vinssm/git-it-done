1780c4ace4e1dcf08037fb4e383b38bfa42bd346
Vinay Vallabhaneni <vinssmedia@gmail.com>
2021-12-05 20:12:49 -0500

    added few more

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,72 +1,71 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
-
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
 var formSubmitHandler = function(event) {
-    event.preventDefault();
-    // get value from input element
-    var username = nameInputEl.value.trim();
-    if (username) {
-        getUserRepos(username);
-        nameInputEl.value = "";
-    } else {
-        alert("Please enter a GitHub username");
-      }
-    console.log(event);
-    
-  };
+  // prevent page from refreshing
+  event.preventDefault();
 
+  // get value from input element
+  var username = nameInputEl.value.trim();
 
+  if (username) {
+    getUserRepos(username);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username");
+  }
+};
 
 var getUserRepos = function(user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a get request to url
-  fetch(apiUrl).then(function(response) {
-    console.log(response);
-    if (response.ok) {
-    response.json().then(function(data) {
-      console.log(data, "This is coming from getUserRepos Call Back Function");
-      displayRepos(data, user);
-     });
-   } else {
-      alert("Error: GitHub User Not Found");
-    }
-})
+  fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function(data) {
+          console.log(data);
+          displayRepos(data, user);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
     .catch(function(error) {
-        // Notice this `.catch()` getting chained onto the end of the `.then()` method
-        alert("Unable to connect to GitHub");
+      alert("Unable to connect to GitHub");
     });
-
+};
 
 var displayRepos = function(repos, searchTerm) {
-    if (repos.length === 0) {
-        repoContainerEl.textContent = "No repositories found.";
-        return;
-    }
-    console.log(repos, "This is coming from displayRepos Call Back Function");
-    console.log(searchTerm);
+  // check if api returned any repos
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
 
-    // clear old content
-    repoContainerEl.textContent = "";
-    repoSearchTerm.textContent = searchTerm;
+  repoSearchTerm.textContent = searchTerm;
 
-    // loop over repos
-for (var i = 0; i < repos.length; i++) {
+  // loop over repos
+  for (var i = 0; i < repos.length; i++) {
     // format repo name
     var repoName = repos[i].owner.login + "/" + repos[i].name;
-  
+
     // create a container for each repo
     var repoEl = document.createElement("div");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
-  
+
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
-  
+
     // append to container
     repoEl.appendChild(titleEl);
 
@@ -76,22 +75,19 @@ for (var i = 0; i < repos.length; i++) {
 
     // check if current repo has issues or not
     if (repos[i].open_issues_count > 0) {
-    statusEl.innerHTML =
+      statusEl.innerHTML =
         "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
     } else {
-    statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
     }
 
     // append to container
     repoEl.appendChild(statusEl);
-  
+
     // append container to the dom
     repoContainerEl.appendChild(repoEl);
-   }
-  };
+  }
+};
 
-}
-
+// add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
-
-//getUserRepos("lernantino");
