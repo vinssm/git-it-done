c955fc9c81a7f32198187008ac76cf84ff69cc02
Vinay Vallabhaneni <vinssmedia@gmail.com>
2021-12-07 07:04:17 -0500

    buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,8 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var languageButtonsEl = document.querySelector("#language-buttons")
+
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
   event.preventDefault();
@@ -18,6 +20,19 @@ var formSubmitHandler = function(event) {
     nameInputEl.value = "";
   } else {
     alert("Please enter a GitHub username");
+  }
+};
+
+
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language");
+  console.log(language)
+
+  if (language) {
+    getFeaturedRepos(language);
+  
+    // clear old content
+    repoContainerEl.textContent = "";
   }
 };
 
@@ -90,5 +105,24 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        console.log(data)
+        displayRepos(data.items, language);
+      //console.log(response);
+    });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  });
+};
+
+
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
